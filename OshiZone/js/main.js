05e1280d0c0fb065f5aee7268e13b11283960af1
Genta Sihera <127002548+gentasihera@users.noreply.github.com>
2025-04-22 09:33:49 +0700

    Delete OshiZone directory

--- OshiZone/js/main.js
+++
@@ -1,9 +0,0 @@
-// Slideshow autoplay
-const slides = document.querySelectorAll('.slide-img');
-    let current = 0;
-    function showNextSlide() {
-      slides[current].classList.remove('active');
-      current = (current + 1) % slides.length;
-      slides[current].classList.add('active');
-    }
-    setInterval(showNextSlide, 3000);
